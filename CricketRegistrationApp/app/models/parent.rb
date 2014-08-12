2f0cc30390e8f6a2072c9c3a6edbe13dda258bdf
Nathan Muckley <nathan.muckley@gmail.com>
2014-08-12 13:34:23 +1200

    Updated most validations

--- CricketRegistrationApp/app/models/parent.rb
+++ CricketRegistrationApp/app/models/parent.rb
@@ -2,4 +2,21 @@ class Parent < ActiveRecord::Base
     has_many :players
     belongs_to :coach, :class_name =>'Coach', :foreign_key =>'coach_id' 
 	accepts_nested_attributes_for :players
+    
+    #Validation
+    validates :p_first_name, :length => {maximum: 50}, presence: true
+    validates :p_surname, :length => {maximum: 50}, presence: true
+    validates :p_address, :length => {maximum: 50}, presence: true
+    validates :p_home_phone, :length => {maximum: 50}, presence: true
+    validates :p_mobile_phone, :length => {maximum: 50}, presence: true
+    validates :p_email, confirmation: true
+    validates :p_email_confirmation, presence: true
+    validates :s_first_name, :length => {maximum: 50}
+    validates :s_surname, :length => {maximum: 50}
+    validates :s_address, :length => {maximum: 50}
+    validates :s_home_phone, :length => {maximum: 50}
+    validates :s_mobile_phone, :length => {maximum: 50}
+    validates :s_email, confirmation: true
+    validates :s_email_confirmation, presence: true
+    #validates :coach_id
 end
