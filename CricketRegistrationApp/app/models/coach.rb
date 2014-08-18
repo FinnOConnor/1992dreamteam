6c6fd1925c5b6b294ace258a60ad41c3330a05fa
Chad Sandeman <chadsandeman@gmail.com>
2014-08-19 11:24:49 +1200

    updated coach validation checks

--- CricketRegistrationApp/app/models/coach.rb
+++ CricketRegistrationApp/app/models/coach.rb
@@ -1,13 +1,15 @@
 class Coach < ActiveRecord::Base
 	has_one :team
     has_one :parent
-	
+	validates_presence_of :mobile_phone, :unless => :home_phone?
+    validates_presence_of :home_phone, :unless => :mobile_phone?
+    
 	#Validation checks
 	validates :email, confirmation: true
     validates :email_confirmation, presence: true
     validates :first_name, :length => {maximum: 50}, presence: true
     validates :surname, :length => {maximum: 50}, presence: true
     validates :address, :length => {maximum: 50}, presence: true
-    validates :home_phone, :length => {maximum: 50}, presence: true
-    validates :mobile_phone, :length => {maximum: 50}, presence: true
+    validates :home_phone, :length => {in: 7..50}, :allow_blank => true 
+    validates :mobile_phone, :length => {in: 7..50}, :allow_blank => true 
 end
