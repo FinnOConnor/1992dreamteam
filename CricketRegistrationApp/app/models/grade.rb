272c774a7e4ab694d5affb39e25534c81f02ab94
Nathan Muckley <nathan.muckley@gmail.com>
2014-08-19 11:53:31 +1200

    Grade validations

--- CricketRegistrationApp/app/models/grade.rb
+++ CricketRegistrationApp/app/models/grade.rb
@@ -1,3 +1,10 @@
 class Grade < ActiveRecord::Base
     has_many :teams
+    
+    #Validations
+    validates :grade_name, :length => {maximum: 50}, presence: true, uniqueness: {case_sensitive: false}
+    validates :min_size, :numericality => { :greater_than => 0, :less_than_or_equal_to => :max_size}
+    validates :max_size, :numericality => { :less_than => 50, :greater_than_or_equal_to => :min_size }
+    validates :min_age, :numericality => { :greater_than => 0, :less_than_or_equal_to => :max_age }
+    validates :max_age, :numericality => { :less_than => 150, :greater_than_or_equal_to => :min_age }
 end
