e05977da36521fd4c0263a6d7eb1fb2482f3f397
Nathan Muckley <nathan.muckley@gmail.com>
2014-09-09 09:59:38 +1200

    Small fix

--- CricketRegistrationApp/app/models/team.rb
+++ CricketRegistrationApp/app/models/team.rb
@@ -5,5 +5,5 @@ class Team < ActiveRecord::Base
 	
 	# Validation checks
 	validates :name, uniqueness: {case_sensitive: false}, :length => {maximum: 50}, presence: true
-    validates :grade, :length => {maximum: 50}, :uniqueness => true, presence: true
+    validates :grade, :numericality => {:greater_than => 0}
 end
