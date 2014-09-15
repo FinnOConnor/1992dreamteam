5a86b27b82431f090cafac2bc3540f8be22d04fd
Callum McCaul <calmac94@hotmail.com>
2014-09-16 10:12:48 +1200

    schema..?

--- CricketRegistrationApp/db/schema.rb
+++ CricketRegistrationApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818223114) do
+ActiveRecord::Schema.define(version: 20140915220853) do
 
   create_table "coaches", force: true do |t|
     t.string   "first_name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20140818223114) do
     t.integer  "home_phone"
     t.integer  "mobile_phone"
     t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "costs", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -91,6 +99,7 @@ ActiveRecord::Schema.define(version: 20140818223114) do
     t.integer  "coach_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "grade_id"
   end
 
 end
