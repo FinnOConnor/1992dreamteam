da3d5c18ae6f1a3cb20a0b07cd55e2328ae5dd5e
Chad Sandeman <chadsandeman@gmail.com>
2014-09-23 13:09:04 +1200

    working on the manage team screen

--- CricketRegistrationApp/app/controllers/manage_controller.rb
+++ CricketRegistrationApp/app/controllers/manage_controller.rb
@@ -1,9 +1,24 @@
 class ManageController < ApplicationController
+  helper_method :resetCounter, :incrementCounter
+  counter = 0;
+    
   def index
       @search = Team.search(params[:q])
       @search1 = Player.search(params[:q])
       @grades = Grade.all
       @players = @search1.result
      @teams = @search.result
+      
+      @counter = @counter
+      
+      
   end
+    
+  def incrementCounter
+      @counter = @counter + 1
+  end 
+    
+  def resetCounter
+      @counter = 0
+  end    
 end
